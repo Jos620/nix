f60136a2ac0eda18a830f399190a161d831b7e95
Mateus Ito <mateusito10@gmail.com>
2025-04-29 17:13:02 -0300

    feat: add more packages

--- flake.nix
+++ flake.nix
@@ -13,14 +13,13 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
-  let
-    configuration = { pkgs, config, ... }: {
-      nixpkgs.config.allowUnfree = true;
+    let
+      configuration = { pkgs, config, ... }: {
+        nixpkgs.config.allowUnfree = true;
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [
           # Utils
           pkgs.mkalias
           pkgs.lua
@@ -32,9 +31,10 @@
           pkgs.python3
           pkgs.pass
           pkgs.gnupg
+          pkgs.nixfmt-classic
+          pkgs.go
 
           # GUI
-          pkgs.alacritty
           pkgs.obsidian
           # pkgs.ghostty
 
@@ -42,6 +42,7 @@
           pkgs.neovim
           pkgs.fish
           pkgs.tmux
+          pkgs.sesh
           pkgs.openssl
           pkgs.btop
           pkgs.curl
@@ -58,100 +59,102 @@
           pkgs.zoxide
           pkgs.gum
           pkgs.volta
+          pkgs.cmatrix
         ];
 
-      homebrew = {
-        enable = true;
-        brews = [
-          "rust"
-          "asdf"
-          "sesh"
-        ];
-        casks = [
-          "ghostty"
-        ];
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
-      };
-
-      fonts.packages = [
-        pkgs.nerd-fonts.jetbrains-mono
-      ];
-
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
+        homebrew = {
+          enable = true;
+          brews = [ 
+            "rust"
+            "asdf"
+          ];
+          casks = [ 
+            "ghostty"
+            "claude"
+            "anytype"
+            "dbeaver-community"
+            "legcord"
+            "bruno"
+          ];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
         };
-      in
 
-      pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read -r src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-      '';
+        fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
 
-      system.defaults = {
-        finder.FXPreferredViewStyle = "clmv";
-        NSGlobalDomain.AppleICUForce24HourTime = true;
-        NSGlobalDomain.AppleInterfaceStyle = "Dark";
-        NSGlobalDomain.KeyRepeat = 2;
-      };
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      programs.zsh.enable = true;
-      programs.fish.enable = true;
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
-
-      users.users.mateusito.shell = pkgs.fish;
-      users.users.mateusito.home = "/Users/mateusito";
-      home-manager.backupFileExtension = "bak";
-      nix.configureBuildUsers = true;
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration 
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.mateusito = import ./home.nix;
-        }
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            user = "mateusito";
-            autoMigrate = true;
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
           };
-        }
-      ];
+
+        in pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+
+        system.defaults = {
+          finder.FXPreferredViewStyle = "clmv";
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain.KeyRepeat = 2;
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        programs.zsh.enable = true;
+        programs.fish.enable = true;
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "x86_64-darwin";
+
+        users.users.mateusito.shell = pkgs.fish;
+        users.users.mateusito.home = "/Users/mateusito";
+        home-manager.backupFileExtension = "bak";
+        nix.configureBuildUsers = true;
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mateusito = import ./home.nix;
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              user = "mateusito";
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
     };
-  };
 }
